dba2799e1eb82a705ccc13233dd3dd06f5cd29f3
Aman Gupta <guptakunalx@gmail.com>
2022-10-25 10:48:41 +0530

    Modify app.js and home.ejs

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+// Global Variables
+const posts = [];
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -19,37 +22,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    homePageContent: homeStartingContent,
-  });
+  res.render("home", { homePageContent: homeStartingContent, postItems: posts });
 });
 
 app.get("/about", function (req, res) {
-  res.render("about", {
-    aboutPageContent: aboutContent,
-  });
+  res.render("about", { aboutPageContent: aboutContent });
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact", {
-    contactPageContent: contactContent,
-  });
+  res.render("contact", { contactPageContent: contactContent });
 });
 
-app.get("/compose", function (req, res) {  
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function (req, res) {  
+app.post("/compose", function (req, res) {
   const newPostTitle = req.body.title;
   const newPostBody = req.body.postBody;
 
   const post = {
     title: newPostTitle,
-    postBody: newPostBody
+    postBody: newPostBody,
   };
 
-  console.log(post);
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
